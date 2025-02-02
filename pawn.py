f1052e4aa85089f47ef9d3847399e2d44d6cea5f
lpark712 <pasadena+lpark@thecoderschool.com>
2025-02-01 17:24:08 -0800

    Update pawn.py

--- pawn.py
+++ pawn.py
@@ -3,6 +3,7 @@ import pygame
 class Pawn:
     def __init__(self, column, row, type, boardspots, boardspots2):
         self.type = type
+        self.firstrow = True
         self.boardspots = boardspots
         self.boardspots2 = boardspots2
         self.pawn = ""
@@ -20,29 +21,21 @@ class Pawn:
         
     def update(self, screen):
         screen.blit(self.pawn, self.rect)
-    def movement(self):
-        press = pygame.key.get_pressed()
-    
-        if press[pygame.K_w] and self.type == "white" and self.row == 6:
-            self.row -= 2
-            self.rect.y = self.boardspots[self.column][self.row][1]
+    def movement(self, position, screen, selected):
+        #add another condition for if the square is occupied and one more for if we clicked on the square to go to
+        if self.select(position, screen, selected):
+            self.rect.y = self.boardspots[self.column][self.row - 1][1]
 
-        elif press[pygame.K_w] and self.type == "white" and self.row > 0:
-            self.row -= 1
-            self.rect.y = self.boardspots[self.column][self.row][1]
-
-        if press[pygame.K_s] and self.type == "black" and self.row == 1:
-            self.row += 2
-            self.rect.y = self.boardspots[self.column][self.row][1]
-
-        elif press[pygame.K_s] and self.type == "black" and self.row < 7:
-            self.row += 1
-            self.rect.y = self.boardspots[self.column][self.row][1]
-    def select(self, position, screen):
+    def select(self, position, screen, selected):
+        if position[0] == self.position[0] - 32.5 and position[1] == self.position[1] - 35 and selected:
+            surface = pygame.Surface((70, 70))
+            surface.fill("yellow")
+            screen.blit(surface, position)
+        else:
+            selected = False
+        return selected
         
-        #move circle into middle of square next time
-        
-        if position[0] == self.position[0] and position[1] == self.position[1] and self.type == "white":
-            pygame.draw.circle(screen, "gray", (self.rect.x, self.boardspots[self.column][self.row - 1][1]), 10)
-        if position[0] == self.position[0] and position[1] == self.position[1] and self.type == "black":
-            pygame.draw.circle(screen, "gray", (self.rect.x, self.boardspots[self.column][self.row + 1][1]), 10)
+    def options(self, whitepawnlist, blackpawnlist):
+        # the pawn lists have objects stored inside them we can check against to see which spots are occupied
+
+        # either 1 giant class or a bunch of different classes for each piece
